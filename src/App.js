8f9d3d37c39c243f1dbcbb1aa715b194a5d480f9
Christopher Tremblay <christopher.tremblay@epfl.ch>
2022-11-22 15:24:21 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const ref = {
+    brut: {
+      horaire: 11.06,
+      mensuel: 1678,
+      annuel: 20136,
+    },
+    net: {
+      horaire: 8.76,
+      mensuel: 1329,
+      annuel: 15950,
+    },
+  };
+  const [relativeSmic, setRelativeSmic] = useState(null);
+  const [options, setOptions] = useState({
+    montant: null,
+    netOuBrut: 'net',
+    temporalite: 'horaire',
+  });
+  const handleChange = (event) => {
+    let toReturn = { ...options, [event.target.name]: event.target.value };
+    setOptions(toReturn);
+  };
+
+  const calcRelative = () => {
+    let transit = ref[options.netOuBrut];
+    if (options.montant === null) setRelativeSmic(0);
+    else
+      setRelativeSmic(
+        (options.montant / transit[options.temporalite]).toFixed(2)
+      );
+  };
+
+  useEffect(() => {
+    calcRelative();
+  }, [options]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='displayTotal'>
+        <input
+          type='text'
+          onChange={handleChange}
+          name='montant'
+          placeholder='Montant €'
+        />{' '}
+        ça fait {relativeSmic} SMIC {options.temporalite} {options.netOuBrut}
+      </div>
+      <div className='options'>
+        <div>
+          C'est du{' '}
+          <select id='netOuBrut' name='netOuBrut' onChange={handleChange}>
+            <option value='net'>Net</option>
+            <option value='brut'>Brut</option>
+          </select>
+          <select id='temporalite' name='temporalite' onChange={handleChange}>
+            <option value='horaire'>Horaire</option>
+            <option value='mensuel'>Mensuel</option>
+            <option value='annuel'>Annuel</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
